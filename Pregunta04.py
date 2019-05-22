d08dbb5fe6069a1a43cd0ec5c3fb41989a465767
Edgard Cuellar <icedgard19701@outlook.es>
2019-05-22 08:28:15 -0500

    Programa Completado

--- Pregunta04.py
+++ Pregunta04.py
@@ -1,6 +1,7 @@
 n =[[1, 11, -5, 3],[3, 22, -2, 4],[5, 10, -6, 8],[9, 16, -1, 3]]
 
-for i in range (0,3):
+for i in range (0,2):
     if n[i+1][i+1] > n[i][i]:
         y = n[i+1][i+1]
-    print(y)
+        
+print(y)
